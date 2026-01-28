a72727b4b242c7de5597484e48a623797fa11de4
juules32 <56916699+Juules32@users.noreply.github.com>
2026-01-28 20:52:56 +0100

    First commit. Uses nightly-2023-06-21

---
+++ src/main.rs
@@ -0,0 +1,52 @@
+use macroquad::prelude::*;
+use minesweeprs::{solve, MineCount, Rule};
+
+#[macroquad::main("BasicShapes")]
+async fn main() {
+
+    let output = solve(
+        &[
+            Rule::new(1, [0, 1]),
+            Rule::new(2, [0, 1, 2]),
+            Rule::new(3, [1, 2, 3]),
+            Rule::new(2, [2, 3, 4]),
+            Rule::new(2, [3, 4, 5, 6, 7]),
+            Rule::new(1, [6, 7, 8]),
+            Rule::new(1, [7, 8]),
+        ],
+        MineCount { total_cells: 85, total_mines: 10 },
+        &-1,
+    );
+
+    println!("{:?}", output);
+    // The board is solvable, so the below should hold:
+    assert_eq!(
+        output,
+        Ok(
+            [
+                (0, 0.0),
+                (1, 1.0),
+                (2, 1.0),
+                (3, 1.0),
+                (4, 0.0),
+                (5, 0.07792207792207793),
+                (6, 0.0),
+                (7, 0.9220779220779222),
+                (8, 0.07792207792207793),
+                (-1, 0.07792207792207792),
+            ].into(),
+        )
+    );
+
+    loop {
+        clear_background(RED);
+
+        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
+        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
+        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
+
+        draw_text("IT WORKS!", 20.0, 20.0, 30.0, DARKGRAY);
+
+        next_frame().await
+    }
+}
